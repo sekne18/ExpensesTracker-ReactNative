b60f52802e6d282e939d767812f9bead892c4e98
JanSekne <sekne50@gmail.com>
2022-08-20 22:39:03 +0200

    user inputs added

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,5 +1,6 @@
 import { useContext, useLayoutEffect } from "react";
 import { StyleSheet, View } from "react-native";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 import Button from "../components/UI/Button";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
@@ -48,14 +49,7 @@ function ManageExpense({route, navigation}) {
 
    return (
       <View style={styles.container}>
-         <View style={styles.buttons}>
-            <Button style={styles.button} mode="flat" onPress={cancelHandler}>
-               Cancel
-            </Button>
-            <Button style={styles.button} onPress={confirmHandler}>
-               {isEditing ? 'Update' : 'Add'}
-            </Button>
-         </View>
+         <ExpenseForm onCancel={cancelHandler} submitButtonLabel={isEditing ? "Update" : "Add"} />
          {isEditing && (
             <View style={styles.deleteContainer}>
                <IconButton 
@@ -77,15 +71,6 @@ const styles = StyleSheet.create({
       flex: 1,
       padding: 24,
       backgroundColor: GlobalStyles.colors.primary800,
-   },
-   buttons: {
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-   },
-   button: {
-      minWidth: 120,
-      marginHorizontal: 8,
    },
    deleteContainer: {
       marginTop: 16,
